b6d1e9a769222a82e9b355d99b8c7fb80602be3d
AshwaniSingh00 <ashwanisingh0742@gmail.com>
2025-06-02 12:53:11 +0530

    Create view1_controller.js

---
+++ UserAuthenticate/view1_controller.js
@@ -0,0 +1,45 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+    ,
+    "sap/m/MessageToast",
+    
+], (Controller,JASONModel,MessageToast) => {
+    "use strict";
+
+    return Controller.extend("project2.controller.View1", {
+        onInit() {
+            
+
+        },
+        validate(inPass){
+            
+            if(inPass.length>=1 && inPass.length<8)
+            {
+                MessageToast.show("Enter Valid Password",{duration: 2000});
+                return false;
+            }
+            else if(inPass.length==0)
+            {
+                MessageToast.show("Enter Password",{duration: 2000});
+                return false;
+            }
+            else{
+                MessageToast.show("Sign In",{duration: 4000});
+                return true;
+            }
+            
+        },
+        onSignIn: function()
+        {
+            var inPass = this.byId("password").getValue();
+            this.validate(inPass);
+            if(this.validate(inPass))
+            {
+                this.getOwnerComponent().getRouter().navTo("View2");
+            }
+            
+        }
+       
+    });
+});
